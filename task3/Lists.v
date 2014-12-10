f941ce3c483a11df288ca5accb6ecdce8e7094c3
tyage <namatyage@gmail.com>
2014-12-10 12:53:38 +0900

    solved hd_opt

--- task3/Lists.v
+++ task3/Lists.v
@@ -1031,16 +1031,19 @@ Definition option_elim (d : nat) (o : natoption) : nat :=
    have to pass a default element for the [nil] case.  *)
 
 Definition hd_opt (l : natlist) : natoption :=
-  (* FILL IN HERE *) admit.
+  match l with
+  | h :: t => Some h
+  | [] => None
+  end.
 
 Example test_hd_opt1 : hd_opt [] = None.
- (* FILL IN HERE *) Admitted.
+  reflexivity. Qed.
 
 Example test_hd_opt2 : hd_opt [1] = Some 1.
- (* FILL IN HERE *) Admitted.
+  reflexivity. Qed.
 
 Example test_hd_opt3 : hd_opt [5;6] = Some 5.
- (* FILL IN HERE *) Admitted.
+  reflexivity. Qed.
 (** [] *)
 
 (** **** Exercise: 1 star, optional (option_elim_hd)  *)
@@ -1049,7 +1052,11 @@ Example test_hd_opt3 : hd_opt [5;6] = Some 5.
 Theorem option_elim_hd : forall (l:natlist) (default:nat),
   hd default l = option_elim default (hd_opt l).
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros l. induction l as [| h t].
+  Case "l = []".
+  reflexivity.
+  Case "l = h t".
+  reflexivity. Qed.
 (** [] *)
 
 (* ###################################################### *)
